0774fae98fc60db5696d60bf51ad530e7e5f9cb1
kitaya <kkitaya@nzigen.com>
2024-02-14 17:02:10 +0900

    first commit: make simple response server

---
+++ server.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+export const prisma = new PrismaClient();
+const app = express();
+
+async function main() {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    })
+  );
+  app.use(express.json());
+
+  app.get("/api/healthchecker", (req: Request, res: Response) => {
+    res.status(200).json({
+      status: "success",
+      message: "Welcome to Two-Factor Authentication with Node.js",
+    });
+  });
+
+  app.all("*", (req: Request, res: Response) => {
+    return res.status(404).json({
+      status: "fail",
+      message: `Route: ${req.originalUrl} not found`,
+    });
+  });
+
+  const PORT = 8000;
+  app.listen(PORT, () => {
+    console.info(`Server starteed on port: ${PORT}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
